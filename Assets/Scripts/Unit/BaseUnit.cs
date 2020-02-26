b8caaced770d330afe79a862350a2dc8bf5b9915
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-02-26 17:51:24 +0900

    BaseUnit実装完了

---
+++ Assets/Scripts/Unit/BaseUnit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public abstract class BaseUnit : MonoBehaviour , IAttackable
+{
+    [SerializeField]
+    private int hitPoint = 1;
+
+    public void Attacked()
+    {
+        hitPoint--;
+
+        if (hitPoint <= 0)
+        { 
+            // TODO:撃破時Effect
+
+            gameObject.SetActive(false);
+            return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var damageable = other.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.ApplyDamage();
+        }
+    }
+}
