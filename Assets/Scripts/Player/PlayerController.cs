1b7899b4b55dd49bdf4c06c352685125aff2d5c0
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-03-01 18:09:11 +0900

    PlayerController実装中

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,5 +2,36 @@
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private PlayerInput _playerInput;
+
+    private PlayerRayCaster _playerRayCaster;
+
+    private PlayerSummoner _playerSummoner;
+
+    private PlayerUnitController _playerUnitController;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+
+        _playerRayCaster = new PlayerRayCaster();
+
+        _playerSummoner = GetComponent<PlayerSummoner>();
+
+        _playerUnitController = new PlayerUnitController();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //入力
+
+        //召喚
+
+        //Unitへ命令
+    }
 }
