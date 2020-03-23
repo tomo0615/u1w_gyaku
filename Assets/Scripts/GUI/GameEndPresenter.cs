bbfc16380293955f29a0130a1fb208b3e4814cb5
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-03-23 21:12:03 +0900

    GameEnd実装中

--- Assets/Scripts/GUI/GameEndPresenter.cs
+++ Assets/Scripts/GUI/GameEndPresenter.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameEndPresenter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameClearView _gameClearView = default;
+
+    [SerializeField]
+    private GameOverView _gameOverView = default;
+
+    public void OnGameEnd(bool isClear)
     {
-        
+        if (isClear)
+        {
+            _gameClearView.ViewGameClear();
+            return;
+        }
+
+        _gameOverView.ViewGameOver();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/GUI/GameOverView.cs
+++ Assets/Scripts/GUI/GameOverView.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class GameOverView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Text gameOverText = default;
 
-    // Update is called once per frame
-    void Update()
+    public void ViewGameOver()
     {
-        
+
     }
 }
