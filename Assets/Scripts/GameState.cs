c5a9571f9aa64c655ea2680ea8430ad692bb6f39
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-03 14:21:43 +0900

    GameStateManagerを実装中

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,6 @@
+﻿public enum GameState
+{
+    Setting,
+    Game,
+    Finish
+}
---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GameStateManager :StateMachine<GameState>
+{
+    [SerializeField]
+    private GameState currentGameState = GameState.Setting;
+
+    private void Awake()
+    {
+        InitializeStateMachine();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void InitializeStateMachine()
+    {
+
+    }
+    //Setting
+    //Loading ->Stageの情報を表示,Unit数を表示-＞Startを表示
+    //上記のUIを別で書く
+    //Game
+    //Playerの操作可能（それ以外のStateでは不可能）
+    //
+    //Finish
+    //Resultー＞戻るなどのボタンなどの表示
+
+    //GameEndは実装済み
+
+}
