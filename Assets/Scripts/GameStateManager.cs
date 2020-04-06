31bb4cfc899f8ec1d12fa2ca5ee7faa8d4a3bbc7
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-07 01:01:09 +0900

    不要なコメント削除

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -79,16 +79,4 @@ public class GameStateManager : StateMachine<GameState>
     }
 
     #endregion 
-
-    //Setting
-    //Loading ->Stageの情報を表示,Unit数を表示-＞Startを表示
-    //上記のUIを別で書く
-    //Game
-    //Playerの操作可能（それ以外のStateでは不可能）
-    //
-    //Finish
-    //Resultー＞戻るなどのボタンなどの表示
-
-    //GameEndは実装済み
-
 }
