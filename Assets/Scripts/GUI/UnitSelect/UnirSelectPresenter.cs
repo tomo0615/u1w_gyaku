8c9118fc79445bcf11c38fd91ea6759ad289d1ea
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-13 20:48:06 +0900

    UnitSelectUIを実装中

---
+++ Assets/Scripts/GUI/UnitSelect/UnirSelectPresenter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class UnirSelectPresenter : MonoBehaviour
+{
+    
+
+    public void Initialize()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlusButtonClicked()
+    {
+        /*
+        if ()
+        {
+
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/GUI/UnitSelect/UnitSelectModel.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+
+public class UnitSelectModel
+{
+    private ReactiveProperty<int> _unitCount;
+
+    public IReadOnlyReactiveProperty<int> UnitCounter
+    {
+        get { return _unitCount; }
+    }
+
+    public UnitSelectModel()
+    {
+        _unitCount = new ReactiveProperty<int>(0);
+    }
+
+    public void SetUnitCount(int count)
+    {
+        _unitCount.Value = count;
+    }
+
+}
