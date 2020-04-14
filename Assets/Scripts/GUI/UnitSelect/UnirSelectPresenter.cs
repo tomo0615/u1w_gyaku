6e43c2de28ffa1c67db9c7a3113dc1a9570a168a
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-14 16:25:07 +0900

    UnitSelectUIを実装中

--- Assets/Scripts/GUI/UnitSelect/UnirSelectPresenter.cs
+++ Assets/Scripts/GUI/UnitSelect/UnirSelectPresenter.cs
@@ -1,29 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UniRx;
 
 public class UnirSelectPresenter : MonoBehaviour
 {
-    
+    [SerializeField]
+    private UnitSelectView _unitSelectView = default;
+
+    private UnitSelectModel _unitSelectModel;
+
+    private const int MAX_COUNT = 100;
+
+    private const int MIN_COUNT = 0;
+
+    private void Awake()
+    {
+        Initialize();
+    }
 
     public void Initialize()
     {
+        _unitSelectModel = new UnitSelectModel();
 
+
+        //Modelの値を監視　変化時Viewに反映
+        _unitSelectModel.UnitCounter
+            .Subscribe()
+            .AddTo(gameObject);
+
+        SetEvents();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void SetEvents()
     {
-        
+        _unitSelectView.OnPlusButtonClickedListener = OnPlusButtonClicked;
+
+        _unitSelectView.OnMinusButtonClickedListener = OnMinusButtonClicked;
     }
 
     public void OnPlusButtonClicked()
     {
-        /*
-        if ()
+        if (_unitSelectModel.UnitCounter.Value < MAX_COUNT)
         {
-
+            _unitSelectModel
+                .SetUnitCount(_unitSelectModel.UnitCounter.Value + 1);
         }
-        */
+    }
+
+    public void OnMinusButtonClicked()
+    {
+        if(_unitSelectModel.UnitCounter.Value > MIN_COUNT)
+        {
+            _unitSelectModel
+                .SetUnitCount(_unitSelectModel.UnitCounter.Value - 1);
+        }
     }
 }
---
+++ Assets/Scripts/GUI/UnitSelect/UnitSelectView.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using System;
+
+public class UnitSelectView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _unitCountText = default;
+
+    public Action OnPlusButtonClickedListener;
+
+    public Action OnMinusButtonClickedListener;
+
+
+    public void OnUnitCountChanged(int count)
+    {
+        _unitCountText.text = count.ToString();
+    }
+
+    public void OnPlusButtonClicked()
+    {
+        OnPlusButtonClickedListener?.Invoke();
+    }
+
+    public void OnMinusButtonClicked()
+    {
+        OnMinusButtonClickedListener?.Invoke();
+    }
+}
