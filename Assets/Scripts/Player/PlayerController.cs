146b56e612ff277fc2e71698d81e8cacabb928dc
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-28 00:14:15 +0900

    不要なコードを削除

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,9 +13,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private UnitType _currentUnitType = UnitType.Normal;
 
-    [SerializeField]
-    private UnitStorageView _unitStorageView = null;
-
     private void Awake()
     {
         Camera camera = Camera.main;
