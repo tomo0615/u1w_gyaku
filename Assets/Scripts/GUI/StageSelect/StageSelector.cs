7272b28512929f6c362ee0fdfee8521d50121330
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-04-30 19:06:56 +0900

    StageSelectorを実装中

---
+++ Assets/Scripts/GUI/StageSelect/StageSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+
+public class StageSelector : MonoBehaviour
+{
+    [SerializeField]
+    private SceneName _stageName = SceneName.Stage1;
+
+    [Inject]
+    private FadeSceneLoader _fadeSceneLoader = default;
+
+
+    private void Start()
+    {
+
+    }
+
+    public void OnSelected()
+    {
+        _fadeSceneLoader.JumpSceneLoad(_stageName);
+    }
+}
