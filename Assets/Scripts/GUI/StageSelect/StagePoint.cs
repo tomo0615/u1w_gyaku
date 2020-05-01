1a0094e0fa40a82ef87cb4d7194a054a11837143
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-01 15:04:25 +0900

    StageSelectギミックの基板を実装

---
+++ Assets/Scripts/GUI/StageSelect/StagePoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class StagePoint : MonoBehaviour , IStagePoint
+{
+    [SerializeField]
+    private SceneName _stageName = SceneName.Stage1;
+
+    [Inject]
+    private readonly FadeSceneLoader _fadeSceneLoader = default;
+
+
+    public void StageSelected()
+    {
+        Debug.Log("hit");
+        //_fadeSceneLoader.JumpSceneLoad(_stageName);
+    }
+}
--- Assets/Scripts/GUI/StageSelect/StageSelector.cs
+++ Assets/Scripts/GUI/StageSelect/StageSelector.cs
@@ -1,25 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Zenject;
 using UniRx;
+using UniRx.Triggers;
 
 public class StageSelector : MonoBehaviour
 {
-    [SerializeField]
-    private SceneName _stageName = SceneName.Stage1;
+    private Camera _camera;
 
-    [Inject]
-    private FadeSceneLoader _fadeSceneLoader = default;
+    private IStagePoint stagePoint;
 
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
 
     private void Start()
     {
+        this.UpdateAsObservable()
+            .Subscribe(_ => GetRayHitStagePoint(Input.mousePosition))
+            .AddTo(this);
 
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonUp(0))
+            .Subscribe(_ => stagePoint.StageSelected())
+            .AddTo(this);
     }
 
-    public void OnSelected()
+    public void GetRayHitStagePoint(Vector3 mousePosition)
     {
-        _fadeSceneLoader.JumpSceneLoad(_stageName);
+        Ray ray = _camera.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        int layerMask = 1 << LayerMask.NameToLayer("StagePoint");
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            stagePoint = hit.transform.gameObject.GetComponent<IStagePoint>();
+        }
+        else
+        {
+            stagePoint = null;
+        }
     }
 }
