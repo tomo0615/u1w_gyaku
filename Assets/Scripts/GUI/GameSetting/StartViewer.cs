453947217eb679d516755092736f921e9e2eb41b
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-05 16:19:12 +0900

    ゲーム開始時のStage名の表示を実装

--- Assets/Scripts/GUI/GameSetting/StartViewer.cs
+++ Assets/Scripts/GUI/GameSetting/StartViewer.cs
@@ -1,12 +1,36 @@
 ﻿using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class StartViewer : MonoBehaviour
 {
     [SerializeField]
     private Animator _animator = default;
 
+    [SerializeField]
+    private TextMeshProUGUI _stageNameText = default;
+
+    private RectTransform _stageNameRect;
+
+    private void Awake()
+    {
+        _stageNameRect = _stageNameText.GetComponent<RectTransform>();
+    }
+
     public void ViewStart()
     {
-        _animator.Play("StartUIAnimation");
+        _stageNameText.text = "Stage1";//テスト用
+
+        _stageNameRect.DOScale(Vector3.one, 0.5f)
+             .OnComplete(() =>
+             {
+                 _stageNameRect.DOScale(Vector3.zero, 0.1f)
+                        .OnComplete(() =>
+                        {
+                            _stageNameText.text = "";
+
+                            _animator.Play("StartUIAnimation");
+                        });
+             });
     }
 }
