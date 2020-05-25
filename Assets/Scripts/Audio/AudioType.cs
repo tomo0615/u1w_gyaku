f4c51f0efc37ede29a8c67a4bb4a0ac8dd99a833
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-25 18:27:51 +0900

    AudioDataクラスを実装

---
+++ Assets/Scripts/Audio/AudioType.cs
@@ -0,0 +1,20 @@
+﻿public enum AudioType
+{
+    titleBGM,
+    gameBGM,
+    resultBGM,
+
+    buttonOK,
+    buttonCancel,
+
+    unitAttack,
+    unitDamage,
+    unitDeath,
+
+    enemyAttack,
+    enemyDamage,
+    enemyDestory,
+
+    win,
+    lose
+}
