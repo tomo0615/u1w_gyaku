3a0b6a21ca8067a51d2a03c4e6c756836e8705d8
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-27 16:21:26 +0900

    UIのAnimationを実装中

--- Assets/Scripts/GUI/UnitSelect/UnitCountView.cs
+++ Assets/Scripts/GUI/UnitSelect/UnitCountView.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 using UniRx;
+using DG.Tweening;
 
 public class UnitCountView : MonoBehaviour
 {
@@ -21,6 +22,11 @@ public class UnitCountView : MonoBehaviour
     private readonly Subject<bool> _onMinus = new Subject<bool>();
     public IObservable<bool> OnMinus() => _onMinus;
 
+    private RectTransform _rectTransform;
+
+    private Tween _punchAnimation;
+
+    private bool isPunching = false;
     public void Initialize()
     {
         _plusButton
@@ -30,10 +36,19 @@ public class UnitCountView : MonoBehaviour
         _minusButton
             .OnClickAsObservable()
             .Subscribe(_ => _onMinus.OnNext(false));
+
+        _rectTransform = GetComponent<RectTransform>();
     }
 
     public void OnUnitCountChanged(int count)
     {
         _unitCountText.text = count.ToString();
+
+        if (isPunching) return;
+
+        isPunching = true;
+
+        _punchAnimation = _rectTransform.DOPunchScale(Vector3.one * 0.5f, 0.5f, 1)
+            .OnComplete(() => isPunching = false);
     }
 }
