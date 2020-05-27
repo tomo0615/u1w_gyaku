f3f3343ebdcc723ea60e3001d6740b864cd4d18d
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-28 02:02:03 +0900

    LoadButtonのリファクタ

--- Assets/Scripts/GUI/Title/LoadButton.cs
+++ Assets/Scripts/GUI/Title/LoadButton.cs
@@ -1,32 +1,18 @@
 ﻿using UnityEngine;
 using Zenject;
-using UniRx;
-using UnityEngine.UI;
 
-public class LoadButton : MonoBehaviour
+public class LoadButton : BaseButton
 {
     [Inject]
     private FadeSceneLoader _fadeSceneLoader = default;
 
-    private Button _button;
-
     [SerializeField]
     private SceneName _jumpSceneName = default;
 
-    private void Awake()
+    public override void OnClicked()
     {
-        _button = GetComponent<Button>();
-    }
+        base.OnClicked();
 
-    private void Start()
-    {
-        _button.OnClickAsObservable()
-            .Subscribe(_ => OnClicked())
-            .AddTo(this);
-    }
-
-    public void OnClicked()
-    {
         _fadeSceneLoader.JumpSceneLoad(_jumpSceneName);
     }
 }
