b672dc620e35e944454beede761e53284cd24efc
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-28 03:26:53 +0900

    Pointerのスプライトを追加

---
+++ Assets/Scripts/Player/PlayerPointer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class PlayerPointer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _spriteTransform = default;
+
+    public void Initialize()
+    {
+        DoPointerAnimation();
+    }
+
+    private void DoPointerAnimation()
+    {
+        _spriteTransform.DOScale(Vector3.one / 1.5f, 1.2f)
+            .OnComplete(() =>
+            {
+                transform.DOScale(Vector3.one * 1.5f, 1.2f);
+            })
+            .SetLoops(-1,LoopType.Yoyo);
+    }
+}
