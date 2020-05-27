4497dba80757fd43660ea4cae7ebdbb9207bac07
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-28 02:00:44 +0900

    BaseButtonを実装

---
+++ Assets/Scripts/GUI/BaseButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BaseButton : MonoBehaviour
+{
+    private Button _button;
+
+    public Button Button
+    {
+        get
+        {
+            if (_button == null)
+            {
+                _button = GetComponent<Button>();
+            }
+            return _button;
+        }
+    }
+
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        _button.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                OnClicked();
+            })
+            .AddTo(gameObject);
+    }
+
+    public virtual void OnClicked()
+    {
+        PlayClickedSound();
+
+        DoButtonClickedAnimation();
+    }
+
+    private void PlayClickedSound()
+    {
+        //Soundnagasu
+    }
+
+    private void DoButtonClickedAnimation()
+    {
+        _rectTransform.DOScale(Vector3.one * 1.1f, 0.1f)
+            .OnComplete(() => _rectTransform.DOScale(Vector3.one, 0.1f));
+    }
+}
