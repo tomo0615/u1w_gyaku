01aa34be57122d2927db5b39e207ab6acb3532d2
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-05-28 15:12:03 +0900

    Pointerをリファクタ

--- Assets/Scripts/Player/PlayerPointer.cs
+++ Assets/Scripts/Player/PlayerPointer.cs
@@ -4,20 +4,28 @@ using DG.Tweening;
 public class PlayerPointer : MonoBehaviour
 {
     [SerializeField]
+    private GameObject _pointerObject = default;
+
+    [SerializeField]
     private Transform _spriteTransform = default;
+
+    [SerializeField]
+    private float _animationTime = 1.2f;
 
     public void Initialize()
     {
+        _pointerObject.SetActive(true);
+
         DoPointerAnimation();
     }
 
     private void DoPointerAnimation()
     {
-        _spriteTransform.DOScale(Vector3.one / 1.5f, 1.2f)
+        _spriteTransform.DOScale(Vector3.one / 1.5f, _animationTime / 2)
             .OnComplete(() =>
             {
-                transform.DOScale(Vector3.one * 1.5f, 1.2f);
+                transform.DOScale(Vector3.one * 1.5f, _animationTime);
             })
-            .SetLoops(-1,LoopType.Yoyo);
+            .SetLoops(-1, LoopType.Yoyo);
     }
 }
