f97b77a119e3ca812643cb02bd741d00eac59c22
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-07-25 21:17:30 +0900

    impl: ボタンのアニメーション部分を独立

---
+++ Assets/Scripts/GUI/ButtonAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using DG.Tweening;
+using Zenject;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(RectTransform))]
+public class ButtonAnimator : MonoBehaviour
+{
+    public enum ButtonAnimType
+    {
+        Punch,   
+    }
+
+    private RectTransform _rectTransform
+    [SerializeField] private ButtonAnimType clickedAnimType;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        var listenerButton = GetComponent<Button>()
+            .OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                Play();
+            })
+            .AddTo(gameObject);
+    }
+
+    private void Play()
+    {
+        switch (clickedAnimType)
+        {
+            case ButtonAnimType.Punch:
+                //TODO: よしなにパラメータ化
+                _rectTransform.DOScale(Vector3.one * 1.1f, 0.1f)
+                    .OnComplete(() => _rectTransform.DOScale(Vector3.one, 0.1f));
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }   
+    }
+}
--- Assets/Scripts/GUI/ButtonSpeaker.cs
+++ Assets/Scripts/GUI/ButtonSpeaker.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using DG.Tweening;
+using Zenject;
 
 [RequireComponent(typeof(Button))]
 public class ButtonSpeaker : MonoBehaviour
